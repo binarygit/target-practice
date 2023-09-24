b1d15be06443567254a8a49072f45e4a6a200e88
binarygit <inscription4@outlook.com>
2023-09-24 13:47:38 +0545

    Refactor fireball logic into separate class

--- main.rb
+++ main.rb
@@ -97,18 +97,60 @@ class Player
   end
 end
 
+class Fireball
+  attr_reader :args
+
+  def initialize(args)
+    @args = args
+  end
+
+  def render
+    args.state.fireballs ||= []
+    if args.inputs.keyboard.key_down.z
+      args.state.fireballs << { x: (args.state.player.x + args.state.player.w) - 12,
+                                y: args.state.player.y + 10,
+                                w: 34, 
+                                h: 34,
+                                path: 'sprites/misc/fireball.png'
+      }
+    end
+    increase_fireball_speed
+    kill_offscreen_fireballs
+
+    args.state.fireballs.reject! { _1.dead }
+    args.outputs.sprites << args.state.fireballs
+  end
+
+  private
+
+  def increase_fireball_speed
+    args.state.fireballs.each do |fireball|
+      fireball.x += args.state.player.speed + 2
+    end
+  end
+
+  def kill_offscreen_fireballs
+    args.state.fireballs.each do |fireball|
+      if fireball.x > args.grid.w
+        fireball.dead = true
+      end
+    end
+  end
+end
+
 class Game
   attr_reader :args, :speed
 
   def initialize(args = nil)
     @args  = args
     @speed = 5
-    @player = Player.new(args)
+    @player   = Player.new(args)
+    @fireballs = Fireball.new(args)
   end
 
   def tick
     @player.render
-    print_fireballs
+    @fireballs.render
     print_targets
     target_hit?
   end
@@ -144,38 +186,6 @@ class Game
       h: 64,
       path: 'sprites/misc/target.png',
     }
-  end
-
-  def print_fireballs
-    args.state.fireballs ||= []
-    if args.inputs.keyboard.key_down.z
-      args.state.fireballs << { x: (args.state.player.x + args.state.player.w) - 12,
-                                y: args.state.player.y + 10,
-                                w: 34, 
-                                h: 34,
-                                path: 'sprites/misc/fireball.png'
-      }
-    end
-
-    increase_fireball_speed
-    kill_offscreen_fireballs
-
-    args.state.fireballs.reject! { _1.dead }
-    args.outputs.sprites << args.state.fireballs
-  end
-
-  def increase_fireball_speed
-    args.state.fireballs.each do |fireball|
-      fireball.x += args.state.player.speed + 2
-    end
-  end
-
-  def kill_offscreen_fireballs
-    args.state.fireballs.each do |fireball|
-      if fireball.x > args.grid.w
-        fireball.dead = true
-      end
-    end
   end
 end
 
